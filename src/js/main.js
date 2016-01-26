79573c5141fe9f1ad069f46d206076a0f8df9f6f
Daniel Klein <danhirschklein@gmail.com>
2016-01-26 10:49:10 -0700

    Skeleton checkout form and basic function

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
-// add scripts
+
 
 $(document).on('ready', function() {
   console.log('sanity check!');
+  $('#billing-same').on('click',function() {
+    var firstName = $("#firstNameShipping").val();
+    var firstNameBilling = $("#firstNameBilling").val(firstName);
+
+    var lastName = $("#lastNameShipping").val();
+    var lastNameBilling = $("#lastNameBilling").val(lastName);
+
+
+
+    });
 });
